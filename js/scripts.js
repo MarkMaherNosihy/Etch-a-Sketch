1e08a9208d6774fbca9d22ce5b51e444ab45a88b
Mark Maher Noshey Aziz <markdevs159@gmail.com>
2023-07-05 08:43:03 +0300

    Added changing grid size feature using range slider and implemented the clear button function

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,10 @@ const defaultColor = 'black';
 const defaultGridSize = 16;
 
 const grid = document.querySelector(".grid");
-
+let rangeSliderVal = document.getElementById("myRange").value;
+const rangeSlider = document.getElementById("myRange");
+const gridSizeText = document.getElementById("grid-size");
+const  clearBtn = document.getElementById("clear-btn");
 let isMouseDown = false;
 
 grid.addEventListener('mousedown', () =>{
@@ -27,20 +30,29 @@ grid.addEventListener('dragstart', (event) => {
 
 function generateGrid(dim = 16)
 {
-    const cellSize = 800 / dim;
+    const cellSize = 500 / dim;
     //Getting the grid and creating a cell element
-    for(let i = 0; i < dim; i++)
+    for(let i = 0; i < dim*dim; i++)
     {
-        for(let j = 0; j < dim; j++)
-        {
             const cell = document.createElement('div');
             cell.addEventListener('mouseover', changeColor);
             cell.addEventListener('mousedown', changeColor);
             cell.style.cssText = `width: ${cellSize}px; height: ${cellSize}px`;
             grid.appendChild(cell);
-        }
     }    
 }
+function clearGrid()
+{
+    grid.innerHTML = "";
+    generateGrid(rangeSliderVal);
+}
+function checkForGridChange(e)
+{
+        rangeSliderVal = e.target.value;
+        clearGrid();
+        gridSizeText.textContent = `${rangeSliderVal} X ${rangeSliderVal}`;
+}
 
-
+rangeSlider.addEventListener('input', checkForGridChange);
+clearBtn.addEventListener('click', clearGrid);
 generateGrid();
